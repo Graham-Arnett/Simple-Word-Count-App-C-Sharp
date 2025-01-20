ca7727162e20ccb41d0cded2ef8b674829fcd354
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-20 13:36:40 -0500

    init commit, this is a work in progress it's not done yet

---
+++ WordCountApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace WordCountApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my sentence word counting app!");
+            string choice;
+            int wordCount = 0;
+            do
+            {
+                Console.Write("\n\nPlease enter a sentence: ");
+                string yourSentence = Console.ReadLine();
+
+                for (int i = yourSentence.Length - 1; i >= 0; i--)
+                {
+                    if (yourSentence[i] == ' ')
+                    {
+                        wordCount++;
+                    }
+
+                }
+                Console.WriteLine($"Your word count is {wordCount} words.");
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}
