e2f928853ac1d13a1145f7dd00dd14a74a5a55da
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-20 15:38:37 -0500

    thought I was doing something here, the count is wildly broken but I'm still pushing to show progress

--- WordCountApp/Program.cs
+++ WordCountApp/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Welcome to my sentence word counting app!");
             string choice;
+            int spaceCount = 0;
             int wordCount = 0;
             do
             {
@@ -16,14 +17,18 @@
                 {
                     if (yourSentence[i] == ' ')
                     {
-                        wordCount++;
+                        spaceCount++;
                     }
 
                 }
-                Console.WriteLine($"Your word count is {wordCount} words.");
+                //wordCount = spaceCount + 1; //if there are no spaces it's one word
+                //Console.WriteLine($"Your word count is {wordCount} words.");
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 choice = Console.ReadLine().ToLower();
             } while (choice == "y");
+            wordCount = spaceCount + 1;
+            wordCount = spaceCount + 1; //if there are no spaces it's one word
+            Console.WriteLine($"Your word count is {wordCount} words.");
         }
     }
 }
