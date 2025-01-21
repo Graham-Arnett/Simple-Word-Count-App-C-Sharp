807e57dd2b4a88e3e1424c8d1990aa0f43fac0cd
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-21 10:48:51 -0500

    its a bit of a dumb solution, but it seems to work now

--- WordCountApp/Program.cs
+++ WordCountApp/Program.cs
@@ -6,10 +6,12 @@
         {
             Console.WriteLine("Welcome to my sentence word counting app!");
             string choice;
-            int spaceCount = 0;
-            int wordCount = 0;
+            //int spaceCount = 0;
+            //int wordCount = 0;
             do
             {
+                int spaceCount = 0;
+
                 Console.Write("\n\nPlease enter a sentence: ");
                 string yourSentence = Console.ReadLine();
 
@@ -21,14 +23,14 @@
                     }
 
                 }
-                //wordCount = spaceCount + 1; //if there are no spaces it's one word
-                //Console.WriteLine($"Your word count is {wordCount} words.");
+                int wordCount = spaceCount + 1; //if there are no spaces it's one word
+                Console.WriteLine($"Your word count is {wordCount} words.");
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 choice = Console.ReadLine().ToLower();
             } while (choice == "y");
-            wordCount = spaceCount + 1;
-            wordCount = spaceCount + 1; //if there are no spaces it's one word
-            Console.WriteLine($"Your word count is {wordCount} words.");
+            //int wordCount = spaceCount + 1;
+            //wordCount = spaceCount + 1; //if there are no spaces it's one word
+            //Console.WriteLine($"Your word count is {wordCount} words.");
         }
     }
 }
